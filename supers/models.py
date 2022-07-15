29156219060f6530c62825850656681d6fef3ba2
unknown <mbisovskyi@gmail.com>
2022-07-15 15:58:46 -0400

    Supers model is modified + CRUD requests for Supers are completed

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from super_types.models import SuperType
 
 # Create your models here.
+class Super(models.Model):
+    name = models.CharField(max_length = 32)
+    alter_ego = models.CharField(max_length = 64)
+    primary_ability = models.CharField(max_length = 64)
+    secondary_ability = models.CharField(max_length = 64)
+    catchphrase = models.CharField(max_length = 128)
+    super_type = models.ForeignKey(SuperType, on_delete = models.CASCADE)
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import SuperSerializer
+from .models import Super
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    if request.method == 'GET':
+        supers_param = request.query_params.get('type')
+        supers = Super.objects.all()
+        if supers_param:
+            supers = supers.filter(super_type__super_type = supers_param)
+        serializer = SuperSerializer(supers, many = True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_by_id(request, pk):
+    super = get_object_or_404(Super, pk = pk)
+    if request.method == 'GET':
+        super_serializer = SuperSerializer(super)
+        return Response(super_serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
