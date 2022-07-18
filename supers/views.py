42238fb3150e4066020175bc9a58ac4b4c05851b
unknown <mbisovskyi@gmail.com>
2022-07-18 14:43:03 -0400

    Response updated for all Super Types request to show list of Heroes and list of Villains

--- supers/views.py
+++ supers/views.py
@@ -6,15 +6,35 @@ from .models import Super
 from rest_framework import status
 
 # Create your views here.
+
+
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
+        heroes = Super.objects.filter(super_type_id = 1)
+        heroes_list = []
+        for hero in heroes:
+            heroes_list.append(hero)
+        villains = Super.objects.filter(super_type_id = 2)
+        villains_list = []
+        for villain in villains:
+                    villains_list.append(villain)
         supers_param = request.query_params.get('type')
-        supers = Super.objects.all()
         if supers_param:
-            supers = supers.filter(super_type__super_type = supers_param)
-        serializer = SuperSerializer(supers, many = True)
-        return Response(serializer.data)
+            if supers_param == 'hero':
+                serializer = SuperSerializer(heroes_list, many = True)
+                return Response(serializer.data)
+            elif supers_param == 'villain':
+                serializer = SuperSerializer(villains_list, many = True)
+                return Response(serializer.data)
+            else:
+                answer = 'No Super Type found'
+                return Response(answer, status = status.HTTP_204_NO_CONTENT)
+        else:
+            heroes = heroes_dict(heroes_list)
+            villains = villains_dict(villains_list)
+            custom_response = {"Heroes": heroes, "Villains": villains}
+            return Response(custom_response)            
     elif request.method == 'POST':
         serializer = SuperSerializer(data = request.data)
         serializer.is_valid(raise_exception = True)
@@ -35,3 +55,29 @@ def super_by_id(request, pk):
     elif request.method == 'DELETE':
         super.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
+
+def heroes_dict(heroes):
+    heroes_dict = []
+    for hero in heroes:
+        hero = {
+        "name": hero.name,
+        "alter_ego": hero.alter_ego,
+        "primary_ability": hero.primary_ability,
+        "secondary_ability": hero.secondary_ability,
+        "catchphrase": hero.catchphrase,
+        }
+        heroes_dict.append(hero)
+    return heroes_dict
+
+def villains_dict(villains):
+    villains_dict = []
+    for villain in villains:
+        villain = {
+        "name": villain.name,
+        "alter_ego": villain.alter_ego,
+        "primary_ability": villain.primary_ability,
+        "secondary_ability": villain.secondary_ability,
+        "catchphrase": villain.catchphrase,
+        }
+        villains_dict.append(villain)
+    return villains_dict
